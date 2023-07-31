5dd563eab5d7e4328996937918c3bc2286ffb220
Miles Mosweu <tmtmosweu@gmail.com>
2023-07-31 20:19:46 +0200

    Add recipes destroy action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,21 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :update, :destroy]
+
   def index
     @recipes = current_user.recipes.order(created_at: :desc)
+  end
+
+  def destroy
+    if @recipe.destroy
+      redirect_to(request.referrer || recipes_path, notice: "Recipe deleted successfully")
+    else
+      redirect_to(request.referrer || recipes_path, alert: "Error: Recipe could not be deleted")
+    end
+  end
+
+  private
+
+  def set_recipe
+    @recipe = current_user.recipes.find(params[:id])
   end
 end
