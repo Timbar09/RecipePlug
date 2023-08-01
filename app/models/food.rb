1248e47aac518b01355d83b612e5ad1e32ee1901
Miles Mosweu <tmtmosweu@gmail.com>
2023-08-01 08:40:44 +0200

    Change foreign key owner_id to user_id

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,7 +3,7 @@ class Food < ApplicationRecord
   has_many :recipes, through: :food_recipes
 
   validates :name, presence: true, length: { minimum: 2, maximum: 50 }
-  validates :measurement_unit, presence: true
+  validates :measurement_unit, presence: true, length: { maximum: 20 }
   validates :price, presence: true, numericality: { decimal: true, greater_than: 0 }
   validates :quantity, presence: true, numericality: { decimal: true, greater_than: 0 }
 end
--- db/migrate/20230731170622_rename_owner_id_to_user_id_in_foods.rb
+++
@@ -1,8 +0,0 @@
-class RenameOwnerIdToUserIdInFoods < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :foods, :owner_id, :user_id
-    rename_column :recipes, :owner_id, :user_id
-    add_index :foods, :user_id
-    add_index :recipes, :user_id
-  end
-end
